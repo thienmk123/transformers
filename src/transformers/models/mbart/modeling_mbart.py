81132927b01cb0b2c5292ccecd0f72ff1f90d2ef
virusser1109 <virusser1109@gmail.com>
2022-10-15 16:43:01 +0000

    fix LongSelfAttn

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -311,15 +311,15 @@ class MBartLongSelfAttention(nn.Module):
         if remove_from_windowed_attention_mask is not None:
             # This implementation is fast and takes very little memory because num_heads x hidden_size = 1
             # from (batch_size x seqlen) to (batch_size x seqlen x num_heads x hidden_size)
-            assert len(remove_from_windowed_attention_mask.size()) == 2, f"con cặc gì đây {remove_from_windowed_attention_mask.size()}"
-            remove_from_windowed_attention_mask = remove_from_windowed_attention_mask[:, :, None, None]
+            
+            remove_from_windowed_attention_mask = remove_from_windowed_attention_mask.squeeze(0).squeeze(0)[:, :, None, None]
 
             # cast to fp32/fp16 then replace 1's with -inf
             float_mask = remove_from_windowed_attention_mask.type_as(q).masked_fill(
                 remove_from_windowed_attention_mask, -10000.0
             )
             ones = float_mask.new_ones(size=float_mask.size())  # tensor of ones
-            assert len(ones.size()) == 4, "vẫn đéo phải 4 ư ?"
+
             # diagonal mask with zeros everywhere and -inf inplace of padding
             d_mask = self._sliding_chunks_matmul_qk(ones, float_mask, self.one_sided_attention_window_size)
             attn_weights += d_mask
