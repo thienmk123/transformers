d12111ee94510b0eb57c2ba6672442f7d11b0cca
virusser1109 <virusser1109@gmail.com>
2022-10-15 16:26:29 +0000

    fix LongSelfAttn

--- src/transformers/models/mbart/modeling_mbart.py
+++ src/transformers/models/mbart/modeling_mbart.py
@@ -311,7 +311,7 @@ class MBartLongSelfAttention(nn.Module):
         if remove_from_windowed_attention_mask is not None:
             # This implementation is fast and takes very little memory because num_heads x hidden_size = 1
             # from (batch_size x seqlen) to (batch_size x seqlen x num_heads x hidden_size)
-            assert len(remove_from_windowed_attention_mask.size()) == 2, "Địt mẹ tại sao k phải 2 ?"
+            assert len(remove_from_windowed_attention_mask.size()) == 2, f"con cặc gì đây {remove_from_windowed_attention_mask.size()}"
             remove_from_windowed_attention_mask = remove_from_windowed_attention_mask[:, :, None, None]
 
             # cast to fp32/fp16 then replace 1's with -inf
